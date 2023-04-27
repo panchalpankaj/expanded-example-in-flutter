d22b1039270f813ce07878aabebc6018cca037e4
Your Name <you@example.com>
2023-04-27 13:22:07 +0530

    this is first commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+        home: Scaffold(
+        body: SafeArea(
+          child: Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(20),
+                color: Colors.red,
+                child: Text('1'),
+              ),
+          Expanded(
+            flex: 7,
+            child:Container(
+
+                padding: EdgeInsets.all(20),
+                color: Colors.blue,
+                child: Text('2'),
+              ),
+          ),
+              Expanded(
+                flex: 3,
+              child: Container(
+
+                padding: EdgeInsets.all(20),
+                color: Colors.green,
+                child: Text('3'),
+              ),
+              )
+            ],
+          ),
+        )
+    ),
+    );
+  }
+}
+
